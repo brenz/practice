7e4d89b25e7d98f5b726b56af7c229f3399f9609
Hong Zhang <hong.zhang3@xerox.com>
2018-07-24 16:15:56 -0500

    itr submission

---
+++ itr.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  var result = "";
+  var matrix = [["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+  ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+  ["M", "MM", "MMM", "", "", "", "", "", ""]]
+  var numString = num + "";
+  var lng = numString.length;
+  for (var x in numString) {
+    if (numString[x] != "0") {
+      result = result + matrix[lng - x - 1][numString[x] - 1];
+    }
+  }
+
+  return result;
+};
+
+console.log(intToRoman(process.argv[2]))
+
+
