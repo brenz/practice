e564a9c774e49a3a7218c64238c975622f2b1865
Hong Zhang <hong.zhang3@xerox.com>
2018-07-24 15:07:18 -0500

    accept code 1

--- rti.js
+++ rti.js
@@ -6,38 +6,43 @@ var romanToInt = function (s) {
     let result = 0;
     let length = s.length - 1;
     for (let i = length; i >= 0; i--) {
-        let num=s.charAt(i);
-        console.log(num)
-        let intNum=isRoman(num);
-        if (intNum==0){
+        let num = s.charAt(i);
+        let perviousNum = s.charAt(i + 1);
+        //console.log(perviousNum)
+        let intNum = isRoman(num);
+        let intPerNum = isRoman(perviousNum);
+        if (intNum == 0) {
             console.log("Not Valid Roman Number");
             return;
         }
-        result=result+intNum;
-        console.log(result);
+        if (intNum >= intPerNum) {
+            result = result + intNum;
+        }else{
+            result = result - intNum;
+        }
     }
-
+    return result;
 };
-var isRoman = function(n){
-    if (n=="I"){
+var isRoman = function (n) {
+    if (n == "I") {
         return 1;
     }
-    if (n=="V"){
+    if (n == "V") {
         return 5;
     }
-    if (n=="X"){
+    if (n == "X") {
         return 10;
     }
-    if (n=="L"){
+    if (n == "L") {
         return 50;
     }
-    if (n=="C"){
+    if (n == "C") {
         return 100;
     }
-    if (n=="D"){
+    if (n == "D") {
         return 500;
     }
-    if (n=="M"){
+    if (n == "M") {
         return 1000;
     }
     return 0;
