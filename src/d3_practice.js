617543976c395f2adbcbaa81b09316b78ccfcba4
Hong Zhang <hong.zhang3@xerox.com>
2018-07-28 17:45:48 -0500

    server done

--- src/d3_practice.js
+++ src/d3_practice.js
@@ -1,12 +1,10 @@
-var d3 = require("d3");
 
 var w=300;
 var h=100;
 var padding=2;
 var dataSet=[5, 10, 14, 20, 25];
 
-var svg =
-    d3.select("body")
+var svg = d3.select("body")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
@@ -20,3 +18,4 @@ svg.selectAll("rect")
    .attr("width", w/dataSet.length - padding)
    .attr("height",(d) => {return d*4 })
    .attr("fill",(d)=>{return "rgb(" + (d*10) + ", 0, 0)"; });
+
---
+++ src/srcServer.js
@@ -0,0 +1,12 @@
+
+var express = require("express");
+var path = require("path");
+
+var app=express();
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, "d3_practice.html"));
+});
+
+app.use(express.static(path.join(__dirname, '.')));
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
