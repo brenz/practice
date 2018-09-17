9f48b3b1b3e7a8b16288ecbedc31b4f326e49d3b
Hong Zhang <hong.zhang3@xerox.com>
2018-09-17 11:10:34 -0500

    push

--- src/srcServer.js
+++ src/srcServer.js
@@ -5,7 +5,7 @@ var path = require("path");
 var app=express();
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, "d3_practice.html"));
+  res.sendFile(path.join(__dirname, "kp_flame.html"));
 });
 
 app.use(express.static(path.join(__dirname, '.')));
