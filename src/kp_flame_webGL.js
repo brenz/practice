7c48d9c081cb8ada4d7f4032b5f54f965fdc7beb
Hong Zhang <hong.zhang3@xerox.com>
2018-10-09 09:20:45 -0500

    base

--- src/kp_flame_webGL.js
+++ src/kp_flame_webGL.js
@@ -12,33 +12,40 @@ var SCREEN_WIDTH = window.innerWidth,
   windowHalfY = window.innerHeight / 2,
   camera, scene, renderer, composer, lightHelper, stats,
   spotLight, spotLights = [],
-  flame,flameGeometry,
+  ambientLight,
+  flame, flameGeometry,
   shinDots = [],
   cameraPositions = [],
   cameraInter = 0,
-  rotateTweenL, rotateTweenR
+  rotateTweenL, rotateTweenR, moveCameraTween,
+  body = document.body,
+  x0 = null,
+  scroll0 = 0
 
 /*
 * TODO: parameter for GUI panel remove in production
 */
 var params = {
+  color: [255, 255, 255],
   exposure: 1,
   bloomStrength: 1.2,
   bloomThreshold: 0.4,
   bloomRadius: 0,
-  rotateY:0,
+  rotateY: 0,
 };
 
-if ( WEBGL.isWebGLAvailable() === true){
+/*
+* WEBGL Supportive detecter
+*/
+if (WEBGL.isWebGLAvailable() === true) {
   // User can have full exprience with WebGl, go to WEBGL init()
   init();
   animate();
 }
 else {
   // go with low bandwidth option;
-  document.getElementById('flame-container').appendChild( WEBGL.getWebGLErrorMessage() );
+  document.getElementById('flame-container').appendChild(WEBGL.getWebGLErrorMessage());
 }
-
 
 /*
 * Load once,
@@ -68,23 +75,21 @@ function init() {
 
   // 2. Create Spotlight https://threejs.org/docs/#api/en/lights/SpotLight
   // TODO: Add more light
-  for (var i=0;i<1;i++){
-    spotLight = new THREE.SpotLight(0xffffff, 10);
-    spotLight.position.set(0,0, 500);
+  for (var i = 0; i < 1; i++) {
+    spotLight = new THREE.SpotLight(0xffffff, 100);
+    spotLight.position.set(0, 0, 500);
     spotLight.angle = Math.PI / 4;
     spotLight.penumbra = 0.05;
     spotLight.decay = 2;
     spotLight.distance = 1000;
     spotLights.push(spotLight);
-    scene.add( spotLight );
+    scene.add(spotLight);
 
-    //lightHelper = new THREE.SpotLightHelper(spotLight);
+    lightHelper = new THREE.SpotLightHelper(spotLight);
     //scene.add( lightHelper);
   }
 
-
-
-  var ambientLight = new THREE.AmbientLight(0xffffff);
+  ambientLight = new THREE.AmbientLight(0xaaeeff);
   scene.add(ambientLight);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -99,19 +104,19 @@ function init() {
   loader.load('./data/KPSM_flame.json', function (g, m) {
     // Create 10 layers by normal geomertery
     g.scale(500, 500, 500);
-    var gg=g.clone()
+    var gg = g.clone()
     for (var i = 0; i < 10; i++) {
-      var scaleSize=1-i*0.01;
-      g.merge(gg.clone().translate(0,0,-i*4).scale(scaleSize,scaleSize,scaleSize));
+      var scaleSize = 1 - i * 0.01;
+      g.merge(gg.clone().translate(0, 0, -i * 4).scale(scaleSize, scaleSize, scaleSize));
     }
 
     // Create material
     // https://threejs.org/docs/index.html#api/en/materials/MeshPhongMaterial
     // ## MeshPhongMaterial - A material for shiny surfaces with specular highlights.
-    var mashPhoneMatrial = new THREE.MeshPhongMaterial( {
+    var mashPhoneMatrial = new THREE.MeshPhongMaterial({
       color: 0xcccccc, specular: 0xffffff, shininess: 250,
       side: THREE.DoubleSide, vertexColors: THREE.VertexColors
-    } );
+    });
 
     // Create Flame scuplture - convert normal Geomertery into buffergeometry
     // https://threejs.org/examples/?q=buffer#webgl_buffergeometry
@@ -119,13 +124,13 @@ function init() {
     var positions = [];
     var normals = [];
     var colors = [], color = new THREE.Color();
-    var d , d2;
+    var d, d2;
     var pA = new THREE.Vector3(), pB = new THREE.Vector3(), pC = new THREE.Vector3(),
-        cb = new THREE.Vector3(), ab = new THREE.Vector3();
-    var triangles=g.vertices.length;
+      cb = new THREE.Vector3(), ab = new THREE.Vector3();
+    var triangles = g.vertices.length;
     /* draw triangles base on the position of normal geometery */
-    for ( var j = 0; j < triangles; j ++ ) {
-      d = Math.random()*3; d2 = d / 2;	// individual triangle size
+    for (var j = 0; j < triangles; j++) {
+      d = 2; d2 = d / 2;	// individual triangle size
       // positions
       // TODO: Create better triangle shape or round dot
       var x = g.vertices[j].x;
@@ -140,45 +145,46 @@ function init() {
       var cx = x + Math.random() * d - d2;
       var cy = y + Math.random() * d - d2;
       var cz = z + Math.random() * d - d2;
-      positions.push( ax, ay, az );
-      positions.push( bx, by, bz );
-      positions.push( cx, cy, cz );
+      positions.push(ax, ay, az);
+      positions.push(bx, by, bz);
+      positions.push(cx, cy, cz);
       // flat face normals
-      pA.set( ax, ay, az );
-      pB.set( bx, by, bz );
-      pC.set( cx, cy, cz );
-      cb.subVectors( pC, pB );
-      ab.subVectors( pA, pB );
-      cb.cross( ab );
+      pA.set(ax, ay, az);
+      pB.set(bx, by, bz);
+      pC.set(cx, cy, cz);
+      cb.subVectors(pC, pB);
+      ab.subVectors(pA, pB);
+      cb.cross(ab);
       cb.normalize();
       var nx = cb.x;
       var ny = cb.y;
       var nz = cb.z;
-      normals.push( nx, ny, nz );
-      normals.push( nx, ny, nz );
-      normals.push( nx, ny, nz );
+      normals.push(nx, ny, nz);
+      normals.push(nx, ny, nz);
+      normals.push(nx, ny, nz);
       // colors
       // Todo: Adjust color and bloom param meter make it more pretty
-      var vx = 0.5+Math.random()*0.1;
-      var vy = 0.7+Math.random()*0.1;
-      var vz = 0.9+Math.random()*0.1;
-      color.setRGB( vx, vy, vz );
-      colors.push( color.r, color.g, color.b );
-      colors.push( color.r, color.g, color.b );
-      colors.push( color.r, color.g, color.b );
+      var vx = 0.5 + Math.random() * 0.1;
+      var vy = 0.7 + Math.random() * 0.1;
+      var vz = 0.9 + Math.random() * 0.1;
+      color.setRGB(vx, vy, vz);
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
+      colors.push(color.r, color.g, color.b);
     }
     function disposeArray() { this.array = null; }
-    flameGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ).onUpload( disposeArray ) );
-    flameGeometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ).onUpload( disposeArray ) );
-    flameGeometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ).onUpload( disposeArray ) );
+    flameGeometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3).onUpload(disposeArray));
+    flameGeometry.addAttribute('normal', new THREE.Float32BufferAttribute(normals, 3).onUpload(disposeArray));
+    flameGeometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3).onUpload(disposeArray));
 
     // Create flame element.
-    flame = new THREE.Mesh( flameGeometry, mashPhoneMatrial );
+    flame = new THREE.Mesh(flameGeometry, mashPhoneMatrial);
 
     // 4. Add shinny Particles
     var sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
     var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    for (var k=0; k<5; k++){
+    var sectionLength = document.getElementsByTagName("section").length;
+    for (var k = 0; k < sectionLength - 1; k++) {
       var shinDot = new THREE.Mesh(sphereGeometry, sphereMaterial);
       var length = Math.floor(Math.random() * g.vertices.length); // Pick up a random dot to perform
       shinDot.position.add(g.vertices[length]);
@@ -188,26 +194,27 @@ function init() {
 
     // Adjust flame position
     // TODO: Conside responsive design
-    flame.position.z = i * 12;
-    flame.position.y = -50;
+    flame.position.z = 0;
+    flame.position.y = -60;
     flame.position.x = windowHalfX / 2 - 100;
     flame.rotateY(-0.1);
-    flame.scale.multiplyScalar(1.8);
+    flame.scale.multiplyScalar(2);
 
     scene.add(flame);
 
     // 5. Define animation destination, defer cameraPositions
-    cameraPositions.push(new THREE.Vector3(flame.position.x, 0, 1000));
+    cameraPositions.push(new THREE.Vector3(0, 0, 1000));
+    //cameraPositions.push(new THREE.Vector3(flame.position.x, 0, 1000));
     for (var i = 0; i < shinDots.length; i++) {
       cameraPositions.push(new THREE.Vector3(
         shinDots[i].position.x + flame.position.x,
         shinDots[i].position.y,
-        300));
+        250));
     }
 
     // 6. Start Flame rotation, Spotlight animation
     flameRotation();
-    for(var i=0;i<spotLights.length;i++){
+    for (var i = 0; i < spotLights.length; i++) {
       tweenlight(spotLights[i]);
     }
 
@@ -225,10 +232,10 @@ function init() {
 
   // Add effctFocus
   // https://threejs.org/examples/#webgl_points_dynamic
-  var effectFocus = new THREE.ShaderPass( THREE.FocusShader );
-  effectFocus.uniforms[ "screenWidth" ].value = window.innerWidth;
-  effectFocus.uniforms[ "screenHeight" ].value = window.innerHeight;
-	effectFocus.renderToScreen = true;
+  var effectFocus = new THREE.ShaderPass(THREE.FocusShader);
+  effectFocus.uniforms["screenWidth"].value = window.innerWidth;
+  effectFocus.uniforms["screenHeight"].value = window.innerHeight;
+  effectFocus.renderToScreen = true;
 
   composer = new THREE.EffectComposer(renderer);
   composer.setSize(window.innerWidth, window.innerHeight);
@@ -245,21 +252,24 @@ function init() {
   // GUI control
   // TODO: Remove GUI control in real products
   var gui = new dat.GUI();
-  gui.add( params, 'exposure', 0.1, 2 ).onChange( function ( value ) {
-    renderer.toneMappingExposure = Math.pow( value, 4.0 );
-  } );
-  gui.add( params, 'bloomThreshold', 0.0, 1.0 ).onChange( function ( value ) {
-    bloomPass.threshold = Number( value );
-  } );
-  gui.add( params, 'bloomStrength', 0.0, 3.0 ).onChange( function ( value ) {
-    bloomPass.strength = Number( value );
-  } );
-  gui.add( params, 'bloomRadius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
-    bloomPass.radius = Number( value );
-  } );
-  gui.add( params, 'rotateY', -1.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
-    flame.rotation.y = Number( value );
-  } );
+  gui.addColor(params, 'color').onChange(function (value) {
+    //AmbientLight.color
+  })
+  gui.add(params, 'exposure', 0.1, 2).onChange(function (value) {
+    renderer.toneMappingExposure = Math.pow(value, 4.0);
+  });
+  gui.add(params, 'bloomThreshold', 0.0, 1.0).onChange(function (value) {
+    bloomPass.threshold = Number(value);
+  });
+  gui.add(params, 'bloomStrength', 0.0, 3.0).onChange(function (value) {
+    bloomPass.strength = Number(value);
+  });
+  gui.add(params, 'bloomRadius', 0.0, 1.0).step(0.01).onChange(function (value) {
+    bloomPass.radius = Number(value);
+  });
+  gui.add(params, 'rotateY', -1.0, 1.0).step(0.01).onChange(function (value) {
+    flame.rotation.y = Number(value);
+  });
 
 
   // 9. Add EventListener
@@ -270,10 +280,10 @@ function init() {
   })
 }
 
-function animate() {
+function animate(time) {
   requestAnimationFrame(animate);
   render();
-  TWEEN.update();
+  TWEEN.update(time);
   stats.update();
 }
 
@@ -286,33 +296,40 @@ function flameRotation() {
   if (!rotateTweenL && !rotateTweenR) {
     rotateTweenL = new TWEEN.Tween(flame.rotation)
       .to({ y: 0 }, 4000)
-      .easing(TWEEN.Easing.Quadratic.InOut)
+      .easing(TWEEN.Easing.Linear.None)
     rotateTweenR = new TWEEN.Tween(flame.rotation)
-      .to({ y: -0.2}, 4000)
-      .easing(TWEEN.Easing.Quadratic.InOut)
+      .to({ y: -0.2 }, 4000)
+      .easing(TWEEN.Easing.Linear.None)
     rotateTweenL.chain(rotateTweenR);
     rotateTweenR.chain(rotateTweenL);
   }
   rotateTweenL.start();
 }
-function moveCamera() {
+
+function moveCamera(i) {
+  document.getElementById("console").innerHTML = "Current Story // CameraInter:" + i;
   rotateTweenL.stop();
   rotateTweenR.stop();
-  animateVector3(camera.position, cameraPositions[cameraInter], {
+  if (i === undefined) {
+    i = cameraInter;
+  }
+  moveCameraTween = animateVector3(camera.position, cameraPositions[i], {
     duration: 1000,
     easing: TWEEN.Easing.Quadratic.InOut,
-    update: function (d) {},
-    callback: function () {
-      cameraInter++;
-    }
+    update: function (d) { },
+    callback: function () { flameRotation() }
   })
 }
 
 function addMouseEvents() {
-  document.addEventListener('mousemove', onDocumentMouseMove, false);
-  document.addEventListener('touchstart', onDocumentTouchStart, false);
-  document.addEventListener('touchmove', onDocumentTouchMove, false);
-  document.addEventListener('wheel', onDocumentMouseScroll, false);
+  //document.addEventListener('mousemove', onDocumentMouseMove, false);
+  //document.addEventListener('wheel', onDocumentMouseScroll, false);
+  body.addEventListener("scroll", onbodyScroll, false);
+
+  //document.addEventListener('touchstart', touchLock, false);
+  //document.addEventListener('mousedown', touchLock, false);
+  //document.addEventListener('mouseup', touchMove, false);
+  //document.addEventListener('touchend', touchMove, false);
 }
 function removeMouseEvents() {
   document.removeEventListener('mousemove', onDocumentMouseMove, false);
@@ -332,34 +349,68 @@ function onWindowResize() {
 }
 
 function onDocumentMouseMove(event) {
-  mouseX = event.clientX - windowHalfX;
-  mouseY = event.clientY - windowHalfY;
 }
-
-function onDocumentTouchStart(event) {
-  if (event.touches.length > 1) {
-    event.preventDefault();
-    mouseX = event.touches[0].pageX - windowHalfX;
-    mouseY = event.touches[0].pageY - windowHalfY;
+function unify(e) {
+  return e.changedTouches ? e.changedTouches[0] : e
+}
+function touchLock(e) {
+  console.log("in start");
+  x0 = unify(e).clientX
+}
+function touchMove(e) {
+  console.log("in move");
+  if (x0 || x0 === 0) {
+    var dx = unify(e).clientX - x0;
+    var s = Math.sign(dx);
+    console.log(dx);
+    console.log(s);
+    if (moveCameraTween !== undefined && moveCameraTween.isPlaying()) { return 0 }
+    if ((cameraInter > 0 || s < 0) && (cameraInter < cameraPositions.length - 1 || s > 0)){
+      cameraInter += s;
+      moveCamera(cameraInter);
+      scrollPage(cameraInter);
+    }
   }
 }
 
-function onDocumentTouchMove(event) {
-  if (event.touches.length === 1) {
-    event.preventDefault();
-
-    mouseX = event.touches[0].pageX - windowHalfX;
-    mouseY = event.touches[0].pageY - windowHalfY;
-  }
-}
 
 function onDocumentMouseScroll(event) {
-  if (event.deltaY > 0) {
-    camera.position.z += 10 // (1000 - camera.position.y)*event.deltaY*0.05;
-  } else {
-    camera.position.z -= 10 // (1000 - camera.position.y)*event.deltaY*0.05;
-  }
+  //event.preventDefault();
+  var top = body.scrollTop;
+  console.log(top);
+
+  if (moveCameraTween !== undefined && moveCameraTween.isPlaying()) { return 0 }
+  var dx = event.deltaY;
+  var s =  Math.sign(dx);
+  //console.log(dx);
+  console.log(top);
+  if (moveCameraTween !== undefined && moveCameraTween.isPlaying()) { return 0 }
+  /*if ((cameraInter > 1 || s < 0) && (cameraInter < cameraPositions.length - 1 || s > 0)){
+    cameraInter += s;
+    moveCamera(cameraInter);
+    scrollPage(cameraInter);
+  }*/
 }
+function onbodyScroll(event){
+  event.preventDefault();
+  var top = body.scrollTop;
+  var dx =  top - scroll0;
+  var s =  Math.sign(dx);
+  console.log("dx:"+dx);
+  console.log("top:"+top);
+  if (moveCameraTween !== undefined && moveCameraTween.isPlaying()) { return 0 }
+  if ((cameraInter >= 0 || s < 0) && (cameraInter < cameraPositions.length || s > 0)){
+    cameraInter += s;
+    moveCamera(cameraInter);
+    scrollPage(cameraInter);
+  }
+
+
+}
+
+function scrollPage(i) {
+  body.scrollTop = SCREEN_HEIGHT * i;
+};
 
 /*
  * Animates a Vector3 to the target
@@ -390,15 +441,15 @@ function animateVector3(vectorToAnimate, target, options) {
   return tweenVector3;
 }
 
-function tweenlight( light ) {
-  new TWEEN.Tween( light.position ).to( {
-    x: ( Math.random() * 1000 ) + 500,
-    y: ( Math.random() * 2000 ) - 1000,
-    z: ( Math.random() * 300 ) - 150
-  }, 5000 )
-  .easing( TWEEN.Easing.Quadratic.Out )
-  .onComplete(function (){
-    tweenlight(light)
-  })
-  .start();
+function tweenlight(light) {
+  new TWEEN.Tween(light.position).to({
+    x: (Math.random() * 1000) + 500,
+    y: (Math.random() * 2000) - 1000,
+    z: (Math.random() * 50) - 50
+  }, 5000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onComplete(function () {
+      tweenlight(light)
+    })
+    .start();
 }
